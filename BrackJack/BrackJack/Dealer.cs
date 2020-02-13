b251375d387d7e9de52d3b20ad32d273c26a9b54
koda-y <koda-y@evoltech.co.jp>
2020-02-13 17:48:02 +0900

    Dealerクラスが新たに追加された

---
+++ BrackJack/BrackJack/Dealer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack
+{
+	class Dealer : Player
+	{
+		/// <summary>
+		/// Dealerがカードを引く処理
+		/// </summary>
+		/// <param name="card"></param>
+		/// <param name="points"></param>
+		/// <returns></returns>
+		public bool IsDrawDealerOverFlow17(Card card, int points)
+		{
+			if (points >= 17) return true;
+			else 
+			{
+				DrawCard(card);
+				return false;
+			}
+		}
+	}
+}
