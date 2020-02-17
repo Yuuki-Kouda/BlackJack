5d7f5871be949f600a14c06240e8a81ec8cb82a5
koda-y <koda-y@evoltech.co.jp>
2020-02-17 15:40:41 +0900

    コメント微修正

--- BrackJack/BrackJack/Player.cs
+++ BrackJack/BrackJack/Player.cs
@@ -1,9 +1,5 @@
 ﻿namespace BlackJack
 {
-	/// <summary>
-	/// プレイヤー
-	/// </summary>
-	class Player
 	enum PlayerCall
 	{
 		None,
@@ -13,20 +9,15 @@
 	class Player : Players
 	{
 		/// <summary>
-		/// 手札
 		/// プレイヤーコール
 		/// </summary>
-		public Hand Hand { get; set; } = new Hand();
 		public PlayerCall PlayerCall { get; set; } = PlayerCall.None;
 
 		/// <summary>
-		/// 手札初期化
 		/// プレイヤー初期化
 		/// </summary>
-		public void InitializeHand()
 		public void InitializePlayer()
 		{
-			Hand = new Hand();
 			PlayerCall = PlayerCall.None;
 		}
 
