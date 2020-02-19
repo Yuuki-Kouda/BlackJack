4b54b37ee96044e8558a389f6eee67bb9e120a5d
koda-y <koda-y@evoltech.co.jp>
2020-02-19 15:40:50 +0900

    IsFinishedStartDealerDrawnの削除

--- BrackJack/BrackJack/Dealer.cs
+++ BrackJack/BrackJack/Dealer.cs
@@ -9,17 +9,6 @@ namespace BlackJack
 	class Dealer : Player
 	{
 		/// <summary>
-		/// ディーラーが開始時ドローを引いている最中、または引いた直後か
-		/// </summary>
-		public bool IsFinishedStartDealerDraw 
-		{ 
-			get 
-			{
-				if (Hand.HandCards.Count > Two) return true;
-				else return false;
-			}
-		}
-		/// <summary>
 		/// ディーラーの点数は17以上か
 		/// </summary>
 		public bool IsFinishedDealerTurn 
@@ -32,7 +21,6 @@ namespace BlackJack
 		}
 
 		//定数
-		private readonly int Two = 2;
 		private readonly int Seventeen = 17;
 
 		/// <summary>
